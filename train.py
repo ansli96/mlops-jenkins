03ec122194aaaf58d41e17ebfce5c9f0db8ad6bc
Anwer <anwer.slimi@expleogroup.com>
2024-05-14 23:57:58 +0200

    update files

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
 print("#####################################")
 print("This is the train Script from the second push trigger")
-print("I DID IT, ALHAMDOULILLAH")
+print("I DID IT, ALHAMDOULILLAH ^_^ ")
 print("#####################################")
